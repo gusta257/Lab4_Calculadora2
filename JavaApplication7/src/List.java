b669e04e84a71e1b73991b383821f053e61610e6
Gustavo de Leon <gdeleontobias@gmail.com>
2018-02-19 15:07:50 -0600

    listas

--- JavaApplication7/src/List.java
+++ JavaApplication7/src/List.java
@@ -1,3 +1,6 @@
+
+import java.util.Iterator;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -87,5 +90,7 @@ public interface List<E>
    public E remove(int i);
    // pre: 0 <= i < size()
    // post: removes and returns object found at that location
-
+     public Iterator<E> iterator();
+   // post: returns an iterator allowing
+   // ordered traversal of elements in list
 }
